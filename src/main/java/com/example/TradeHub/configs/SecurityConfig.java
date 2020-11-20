b9de9dfb13ef4565b1714d4ebd1ddc756615379f
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-11-20 13:48:15 +0100

    Sass and cleanup

---
+++ src/main/java/com/example/TradeHub/configs/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.TradeHub.configs;
+/*
+        <description>
+        @author Marcus Sandberg
+        @since 2020-11-20
+*/
+
+public class SecurityConfig {
+}
---
+++ src/main/resources/static/frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+
+    return <h1>TradeHub Inc</h1>;
+
+}
+
+export default Home;
