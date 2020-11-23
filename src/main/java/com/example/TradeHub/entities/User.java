6f033f8e71e9e725e0c33384a63e12a9c98c22f0
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-11-23 09:21:33 +0100

    Started working on user backend

---
+++ src/main/java/com/example/TradeHub/entities/User.java
@@ -0,0 +1,35 @@
+package com.example.TradeHub.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.jni.Address;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    private String id;
+    private String email;
+    private String password;
+    private String fullName;
+     // Add when entities has been created
+    // private Address address;
+    //private List<Auction> auctions;
+    //private Company company;
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
