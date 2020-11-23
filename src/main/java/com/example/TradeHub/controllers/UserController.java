2141a2bcfec3f4d807f7ff2bd8f5d9d225536969
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-11-23 09:51:47 +0100

    Started working on userService

--- src/main/java/com/example/TradeHub/controllers/UserController.java
+++ src/main/java/com/example/TradeHub/controllers/UserController.java
@@ -1,8 +1,21 @@
 package com.example.TradeHub.controllers;
 
+import com.example.TradeHub.entities.User;
+import com.example.TradeHub.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/v1/user")
 public class UserController {
+
+@Autowired
+    private UserService userService;
+
+public ResponseEntity<User> addUser(@RequestBody User user){
+    return ResponseEntity.ok(userService.saveUser(user));
+}
 
 }
--- src/main/java/com/example/TradeHub/services/UserService.java
+++ src/main/java/com/example/TradeHub/services/UserService.java
@@ -1,7 +1,12 @@
 package com.example.TradeHub.services;
 
+import com.example.TradeHub.entities.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    public void saveUser(User user){
+
+    }
 }
