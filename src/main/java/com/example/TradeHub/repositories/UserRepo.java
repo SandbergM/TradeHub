2fc0ee57bc0e872eab3f21ee588730710392e590
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-11-23 10:16:13 +0100

    Working on mongoTemplate

--- src/main/java/com/example/TradeHub/repositories/UserRepo.java
+++ src/main/java/com/example/TradeHub/repositories/UserRepo.java
@@ -1,4 +1,23 @@
 package com.example.TradeHub.repositories;
 
+import com.example.TradeHub.entities.User;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Optional;
+
 public class UserRepo {
+    private final MongoTemplate mongoTemplate;
+
+    public UserRepo(MongoTemplate mongoTemplate){
+        this.mongoTemplate = mongoTemplate;
+    }
+    public findAllByEmail(String email){
+        var users = mongoTemplate.findAll(User.class);
+    }
+    public Optional<User> save(User user){
+        return Optional.of(mongoTemplate.save(user));
+    }
+
+
 }
--- src/main/java/com/example/TradeHub/services/UserService.java
+++ src/main/java/com/example/TradeHub/services/UserService.java
@@ -9,5 +9,6 @@ public class UserService {
     public void postNewUser(User user){
 
 
+
     }
 }
