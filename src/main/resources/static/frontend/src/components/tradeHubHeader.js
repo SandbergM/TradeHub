f0aea69c7d4feeedd8268ba89d73f55e5e0fc24b
Martin Hellström <martver12@gmail.com>
2020-11-24 14:34:50 +0100

    Fixed for mobile use

--- src/main/resources/static/frontend/src/components/tradeHubHeader.js
+++ src/main/resources/static/frontend/src/components/tradeHubHeader.js
@@ -24,8 +24,8 @@ const TradeHubHeader = () => {
   return (
     <div>
       <Navbar className="light-grey-background" expand="md">
-        <NavbarBrand className="ml-4 text-dark " href="/">
-          <h2 className="my-auto ml-4">
+        <NavbarBrand className="text-dark " href="/">
+          <h2 className="my-auto">
             Trade<span className="orange-background tradeHub-white">Hub</span>
           </h2>
         </NavbarBrand>
@@ -34,10 +34,10 @@ const TradeHubHeader = () => {
           {/* <NavbarText>Simple Text</NavbarText> */}
         </Collapse>
         <div className="text-right mr-4 tradeHub-icon">
-          <span class="material-icons mr-4">chat</span>
+          <span class="material-icons">chat</span>
         </div>
-        <div className="text-right mr-4 tradeHub-icon">
-          <span class="material-icons mr-4">menu</span>
+        <div className="text-right tradeHub-icon">
+          <span class="material-icons">menu</span>
         </div>
       </Navbar>
     </div>
