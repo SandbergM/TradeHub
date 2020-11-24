029fd6ee2eedf1cf1e83aaf9be270e55c58c4784
Martin Hellström <martver12@gmail.com>
2020-11-24 14:31:14 +0100

    added icons and material icon use in .html

--- src/main/resources/static/frontend/src/components/tradeHubHeader.js
+++ src/main/resources/static/frontend/src/components/tradeHubHeader.js
@@ -25,7 +25,7 @@ const TradeHubHeader = () => {
     <div>
       <Navbar className="light-grey-background" expand="md">
         <NavbarBrand className="ml-4 text-dark " href="/">
-          <h2 className="my-auto">
+          <h2 className="my-auto ml-4">
             Trade<span className="orange-background tradeHub-white">Hub</span>
           </h2>
         </NavbarBrand>
@@ -33,12 +33,15 @@ const TradeHubHeader = () => {
         <Collapse isOpen={isOpen} navbar>
           {/* <NavbarText>Simple Text</NavbarText> */}
         </Collapse>
+        <div className="text-right mr-4 tradeHub-icon">
+          <span class="material-icons mr-4">chat</span>
+        </div>
+        <div className="text-right mr-4 tradeHub-icon">
+          <span class="material-icons mr-4">menu</span>
+        </div>
       </Navbar>
     </div>
   );
-  // <div className="light-grey-background container col-12">
-  //   <h1 className="ml-4 tradeHub-orange">#Logo</h1>
-  // </div>;
 }
 
 export default TradeHubHeader
