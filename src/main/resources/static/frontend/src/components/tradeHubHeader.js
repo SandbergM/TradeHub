2e8c04ed54f4ddb8a26ab9e6eada1318d618e034
theCodeHog <paolatosca@outlook.com>
2020-11-25 17:22:53 +0100

    header fixes added menu & button positioning

--- src/main/resources/static/frontend/src/components/tradeHubHeader.js
+++ src/main/resources/static/frontend/src/components/tradeHubHeader.js
@@ -4,14 +4,14 @@ import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
+  NavItem,
+  NavLink,
+  Nav,
 } from "reactstrap";
 
 const TradeHubHeader = () => {
-
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => setIsOpen(!isOpen);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
@@ -21,19 +21,34 @@ const TradeHubHeader = () => {
             Trade<span className="orange-background tradeHub-white">Hub</span>
           </h2>
         </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          {/* <NavbarText>Simple Text</NavbarText> */}
+          <Nav navbar>
+            <NavItem>
+              <NavLink className="tradeHub-grey" href="/">
+                Hem
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="tradeHub-grey" href="/login">
+                Logga in
+              </NavLink>
+            </NavItem>
+          </Nav>
         </Collapse>
-        <div className="text-right mr-4 tradeHub-icon">
-          <span className="material-icons">chat</span>
-        </div>
-        <div className="text-right tradeHub-icon">
+
+        <div
+          className="text-right tradeHub-icon"
+          onClick={toggle}
+          id="hamburger-position"
+        >
           <span className="material-icons">menu</span>
         </div>
       </Navbar>
+      <div className="text-right mr-4 tradeHub-icon" id="bubble-position">
+        <span className="material-icons">chat</span>
+      </div>
     </div>
   );
-}
+};
 
-export default TradeHubHeader
+export default TradeHubHeader;
--- src/main/resources/static/frontend/src/pages/Home.js
+++ src/main/resources/static/frontend/src/pages/Home.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import LoginModal from '../components/loginModal'
-import { Row } from 'reactstrap';
-import AuctionItem from '../components/AuctionItem'
-import AuctionList from '../components/AuctionList';
+import React from "react";
+import LoginModal from "../components/loginModal";
+import { Row } from "reactstrap";
+import AuctionItem from "../components/AuctionItem";
+import AuctionList from "../components/AuctionList";
+
 const Home = () => {
-return(
+  return (
     <div>
-        <h1 className="text-warning text-center">Auktioner</h1>
-     <AuctionList></AuctionList>
+      <h1 className="text-warning text-center">Auktioner</h1>
+      <AuctionList></AuctionList>
     </div>
-
-)
- 
-
-}
+  );
+};
 
 export default Home;
