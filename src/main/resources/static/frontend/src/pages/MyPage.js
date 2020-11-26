063ab4b665d2b434e8f76210c9166757cb598ee5
Fredrik <Fredrikjay@gmail.com>
2020-11-26 14:00:50 +0100

    my page css completed, is responsive

--- src/main/resources/static/frontend/src/pages/MyPage.js
+++ src/main/resources/static/frontend/src/pages/MyPage.js
@@ -1,33 +1,79 @@
 import React, { useState } from "react";
-import { Row, Col } from "reactstrap";
-import {
-    Dropdown, DropdownMenu, DropdownToggle,Button,DropdownItem
-} from "reactstrap";
+import { Row, Col, CardText } from "reactstrap";
+import { Collapse, Button, CardBody, Card, CardFooter } from "reactstrap";
+import plusImg from '../images/plus.svg'
 
 const MyPage = () => {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isMyAuctionsOpen, setIsMyAuctionsOpen] = useState(false);
+  const [isMyBidsOpen, setIsMyBidsOpen] = useState(false);
+  const [isCreateAuctionOpen, setIsCreateAuctionOpen] = useState(false);
 
-    const toggle = () => setDropdownOpen(prevState => !prevState);
+  const toggleProfile = () => setIsProfileOpen((prevState) => !prevState);
+  const toggleAuctions = () => setIsMyAuctionsOpen((prevState) => !prevState);
+  const toggleBids = () => setIsMyBidsOpen((prevState) => !prevState);
+  const toggleCreateAuction = () => setIsCreateAuctionOpen((prevState) => !prevState);
   return (
-    <Row>
-      <Col>
-        <div>
-          <h1>WELCOME!</h1>
-          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle className="my-dropdown" my-dropdowncaret>
-            MIN PROFIL
-          </DropdownToggle>
-          <DropdownMenu className="my-dropdownenu-menu">
-            <DropdownItem>Mitt namn</DropdownItem>
-            <DropdownItem>EMAIL: </DropdownItem>
-            <DropdownItem>ADRESS:</DropdownItem>
-            <DropdownItem>postal code | city</DropdownItem>
-            <DropdownItem divider />
-            <Button size="lg" block>Lägg Till Företag</Button>
-          </DropdownMenu>
-        </Dropdown>
-        </div>
+    <Row >
+      <Col xs="12" sm="2" md="12"className="mb-3"> 
+      <h3 className="text-center">WELCOME #name</h3>
       </Col>
+        <Col xs="12" sm="2" md="3"className="mb-3">  
+          <Button color="secondary" onClick={toggleProfile} block>
+            MIN PROFIL
+          </Button>
+          <Collapse isOpen={isProfileOpen}>
+            <Card>
+              <CardBody>
+                <CardText>Mitt Namn</CardText>
+                <CardText>Email:</CardText>
+                <CardText>Adress:</CardText>
+                <CardText>postkod | stad</CardText>
+              </CardBody>
+              <CardFooter>
+                <Button block>Lägg Till företag</Button>
+              </CardFooter>
+            </Card>
+          </Collapse>
+        </Col>
+        <Col xs="12" sm="2" md="3"className="mb-3"> 
+          <Button color="secondary" onClick={toggleAuctions} block>
+            MINA AUKTIONER
+          </Button>
+          <Collapse isOpen={isMyAuctionsOpen} >
+            <Card>
+              <CardBody>
+               <CardText>My auctions component goes here</CardText>
+              </CardBody>
+            </Card>
+          </Collapse>
+        </Col>
+        <Col xs="12" sm="2" md="3"className="mb-3"> 
+          <Button color="secondary" onClick={toggleBids} block>
+            MINA BUD
+          </Button>
+          <Collapse isOpen={isMyBidsOpen}>
+            <Card>
+              <CardBody>
+               <CardText>My bids component goes here</CardText>
+              </CardBody>
+            </Card>
+          </Collapse>
+        </Col>
+        <Col xs="12" sm="2" md={{ size: 3, offset: 0 }}className="mb-3">
+          <Button color="warning" onClick={toggleCreateAuction} block>
+           <img src={plusImg}></img>
+            SKAPA EN AUKTION
+          </Button>
+          <Collapse isOpen={isCreateAuctionOpen}>
+            <Card>
+              <CardBody>
+               <CardText>Create auctions component goes here</CardText>
+              </CardBody>
+            </Card>
+          </Collapse>
+        </Col>
+      
     </Row>
   );
 };
