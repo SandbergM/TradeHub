d6a6b0424ad9bdc149336dc5a2d25c2ee599a381
Martin Hellström <martver12@gmail.com>
2020-11-30 10:27:32 +0100

    Added easter egg

--- src/main/resources/static/frontend/src/components/TradeHubFooter.js
+++ src/main/resources/static/frontend/src/components/TradeHubFooter.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const TradeHubFooter = () => {
- 
 
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+ 
+      let debounceID = null;
+      const debounceHelper = () => {
+        if (debounceID !== null) {
+          clearTimeout(debounceID);
+          debounceID = null;
+        }
+        debounceID = setTimeout(() => {
+          setModal(false)
+        }, 600);
+      };
+
+      useEffect(() => {
+        if(modal){
+          debounceHelper()
+        }
+      },[modal])
   return (
     <div className="pt-2">
-      <footer>
-      <hr></hr>
+      <footer onClick={toggle}>
+        <hr></hr>
         <p className="font-weight-bold m-0">&#169; Trade Hub AB 2020</p>
         <p className="m-0">Happy Road 21, 234 67 Los Angeles, USA</p>
-        <p >067 - 345 456 56 | tradehub@hublife.com</p>
+        <p>067 - 345 456 56 | tradehub@hublife.com</p>
       </footer>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle} className="text-center mx-auto">
+          Påsk ägg
+        </ModalHeader>
+        <ModalBody>
+          <p>Daggmask was here</p>
+          <img src="https://memes.memedrop.io/production/eRyN1q0D563m/292w_s.jpg"></img>
+        </ModalBody>
+        <ModalFooter></ModalFooter>
+      </Modal>
     </div>
   );
 };
