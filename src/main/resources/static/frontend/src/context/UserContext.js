254a969f6335930e3f0b6e438b43d0b773be9f46
Fredrik <Fredrikjay@gmail.com>
2020-11-30 16:03:50 +0100

    added required to input fields in login modal and removed console logs

--- src/main/resources/static/frontend/src/context/UserContext.js
+++ src/main/resources/static/frontend/src/context/UserContext.js
@@ -1,18 +1,21 @@
-import React, { createContext, useEffect, useState  } from "react";
+import React, { createContext, useState  } from "react";
 
 export const UserContext = createContext();
 
 const UserContextProvider = (props) => {
     const [user, setUser] = useState(null);
 
+    //Not in use, remove this comment and the fetchUser function if its still not in use in sprint 2.
     const fetchUser = async () => {
         let res = await fetch("/api/v1/users/whoami");
         try {
           res = await res.json();
           if(res.status === 200){
+            console.log(res)
             setUser(res);
           }
           else{
+            console.log("else: ",res)
             setUser(null);
           }
         } catch {
@@ -20,10 +23,6 @@ const UserContextProvider = (props) => {
         }
       };
 
-      useEffect(() =>{
-        console.log("user changed");
-        console.log(user);
-      }, [user])
 
   const values = {
     user,
