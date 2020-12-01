4f581aea262b585cc79aa723feae72760f8fef04
theCodeHog <paolatosca@outlook.com>
2020-12-01 13:23:33 +0100

    added a css spinner

--- src/main/resources/static/frontend/src/components/AuctionList.js
+++ src/main/resources/static/frontend/src/components/AuctionList.js
@@ -1,47 +1,49 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import { Row } from "reactstrap";
 import AuctionItem from "../components/AuctionItem";
-import tuttiPrutti from "../images/346095.png"
+import tuttiPrutti from "../images/346095.png";
 import SearchField from "./searchField";
 
 const AuctionList = () => {
-  const [auctions, setAuctions] = useState([])
+  const [auctions, setAuctions] = useState([]);
 
   useEffect(() => {
-   fetchAuctions()
-  },[])
+    fetchAuctions();
+  }, []);
 
-  const fetchAuctions = async ()=>{
+  const fetchAuctions = async () => {
+    const loader = document.getElementById("loader");
     let res = await fetch("/api/v1/auctions");
     try {
       res = await res.json();
       setAuctions(res);
+      loader.classList.add("hidden");
     } catch {
       console.error("could not fetch auctions");
     }
-  }
+  };
 
   return (
     <div>
-      <SearchField/>
-    <Row xs="1" sm="2" md="3">
+      <SearchField />
+      <div id="loader"></div>
+      <Row xs="1" sm="2" md="3">
         {auctions.map((auction, i) => {
-            return (
-                <AuctionItem
-                key = {i}
-                id = {auction.id}
-                title = {auction.title}
-                description = {auction.description}
-                image = {tuttiPrutti}
-                timestamp = {auction.timestamp}
-                highestBid = {auction.highestBid}
-                price = {auction.price}
-                seller= {auction.seller}
-                >
-                </AuctionItem>
-            ) 
+          return (
+            <AuctionItem
+              key={i}
+              id={auction.id}
+              title={auction.title}
+              description={auction.description}
+              image={tuttiPrutti}
+              timestamp={auction.timestamp}
+              highestBid={auction.highestBid}
+              price={auction.price}
+              seller={auction.seller}
+            ></AuctionItem>
+          );
         })}
-    </Row>
+      </Row>
     </div>
   );
 };
